b5cf3aef7edba00caaeaee59bfd90ffd2b817b31
Louis <louis@DEBIAN-LOUIS-SAULNIER.home>
2024-09-23 16:27:37 +0200

    made exercice 3

---
+++ code/exercice3.py
@@ -0,0 +1,6 @@
+print("Entrer la valeur du côté du cube dont on doit calculer le volume.")
+
+side = input()
+volume = pow(int(side), 3)
+
+print("Le volume du cube est de " + volume + " mètres carrés.")
